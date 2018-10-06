8660ff1c76fa4239a4968d8f8d3b4e5ff3ce3734
MerceyAnderson <43886391+MerceyAnderson@users.noreply.github.com>
2018-10-06 18:18:59 +0300

    Update name_nick_hobby.rb

--- name_nick_hobby.rb
+++ name_nick_hobby.rb
@@ -1,7 +1,5 @@
 # Программа которая выводит ваше имя,ваш ник телеграм,выводит название хобби (или кота)
 
-puts "My name : Eugen".chomp
-puts "My telegram nick : @deGoose".chomp
-puts "My hobby : computer games , read books , history , laze :) ".chomp
-
-
+puts "My name : Eugen"
+puts "My telegram nick : @deGoose"
+puts "My hobby : computer games , read books , history , laze :) "
